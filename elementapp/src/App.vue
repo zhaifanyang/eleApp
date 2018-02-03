20a6f35fb4b2d3bbece7bd74524e1cc39cb3b300
Your Name <403623892@qq.com>
2018-02-03 13:38:48 +0800

    构架

---
+++ elementapp/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <v-header></v-header>
+    <div class="tab">
+      <div class="tab-item"></div>
+      <div class="tab-item"></div>
+      <div class="tab-item"></div>
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  display: flex;
+}
+</style>
